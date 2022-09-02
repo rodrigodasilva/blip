ab0c50fcab0ed30b938e5a555a43d1c37e56af29
Rodrigo Ribeiro <ribeiro.rodrigodasilva@gmail.com>
2022-09-02 00:28:27 -0300

    feat: favorites animation

--- src/pages/ChatbotList/components/CardListWrapper/index.tsx
+++ src/pages/ChatbotList/components/CardListWrapper/index.tsx
@@ -1,17 +1,24 @@
+import React from "react";
+
 import useView from "@/hooks/useView";
 
 import * as S from "./styles";
 
 type CardListProps = { children: React.ReactNode };
 
-const CardListWrapper: React.FC<CardListProps> = ({ children }) => {
+const CardListWrapper: React.ForwardRefRenderFunction<
+  HTMLUListElement,
+  CardListProps
+> = ({ children }, ref) => {
   const { view } = useView();
 
   return (
-    <S.CardGridWrapper className={`card-grid--${view}`}>
+    <S.CardGridWrapper ref={ref} className={`card-grid--${view}`}>
       {children}
     </S.CardGridWrapper>
   );
 };
 
-export default CardListWrapper;
+export default React.forwardRef<HTMLUListElement, CardListProps>(
+  CardListWrapper
+);
